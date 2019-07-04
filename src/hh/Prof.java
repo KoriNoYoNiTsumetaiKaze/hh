f7e2ac3412c9313578c3aa225eb512c0283bead0
KoriNoYoNiTsumetaiKaze <darkanddeep@yandex.ru>
2019-07-04 15:59:09 +0300

    класс проф

---
+++ src/hh/Prof.java
@@ -0,0 +1,11 @@
+package hh;
+
+public class Prof {
+	private String name	= null;
+	private double id	= 0.000;
+	
+	Prof(String name, double id){
+		this.name	= name;
+		this.id		= id;
+	}
+}
---
+++ src/hh/ProfArea.java
@@ -0,0 +1,11 @@
+package hh;
+
+public class ProfArea {
+	private String name	= null;
+	private int id	= 0;
+	
+	ProfArea(String name, int id){
+		this.name	= name;
+		this.id		= id;
+	}
+}
